88faf14520da829d23b9d843fad890f2282d2930
dylber cabrera <dylber.cabrera47878@ucaldas.edu.co>
2024-09-09 08:06:15 -0500

    implementacion funcion de resta

--- main.py
+++ main.py
@@ -2,5 +2,17 @@
 def suma(x, y):
     return x + y
 
+
+def resta(x, y):
+    return x - y
+
 resultado = suma(5, 4)
-print(resultado)
+print("el resultao es: " + resultado)
+resultado2 = resta(5, 4)
+print(resultado2)
+
+#------------------------------ COMANDOS PARA GIT HUB --------------------------------
+#git init   inicializador
+#git add .
+#git commit -m   subo a la nube
+#git log    mirar identificadores
